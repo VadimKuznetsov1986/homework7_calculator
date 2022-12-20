23905800d09b63b093b8c32743fe8f6b1d3018ed
Kuznetsov Vadim <kuzibundius@gmail.com>
2022-12-20 14:37:14 +0700

    finish

---
+++ main.py
@@ -0,0 +1,11 @@
+import view
+import calc_rational
+import calc_complex
+
+mode = view.get_mode()
+num1, sign, num2 = view.get_data()
+if mode == 1:
+    result = calc_rational.calc_value(num1, num2, sign)
+else:
+    result = calc_complex.calc_value(num1, num2, sign)
+view.view_data(result)
